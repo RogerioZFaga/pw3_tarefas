5062337c65ce0287b278474da2e3275bfbe75d55
RogerioZFaga <rogeriozfaga@gmail.com>
2025-08-05 10:38:31 -0300

    aula2

---
+++ pw3Professor/pw3-tarefas/src/main/java/br/com/etechas/tarefas/repository/TarefaRepository.java
@@ -0,0 +1,16 @@
+package br.com.etechas.tarefas.repository;
+
+import br.com.etechas.tarefas.entity.Tarefa;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
+
+}
+
+
+
+
+
+
+
+
